11bab0c03a856f0fa043c3fc0ae31fbc6fbb12d2
Henrik Saega <Henrik.saega@gmail.com>
2026-03-19 20:46:39 +0200

    Save user data

--- src/main/java/com/mycompany/mywebapp/UserController.java
+++ src/main/java/com/mycompany/mywebapp/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import com.mycompany.mywebapp.user.User;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -12,6 +13,8 @@ import java.util.List;
 public class UserController {
 
     @Autowired
+
+
     private com.mycompany.mywebapp.user.UserService service;
 
     @GetMapping("/users")
@@ -19,5 +22,18 @@ public class UserController {
         List<User> listUsers = service.listAll();
         model.addAttribute("listUsers", listUsers);
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String showNewForm(Model model){
+        model.addAttribute("user", new User());
+        return "users_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user){
+        service.save(user);
+        
+        return "redirect:/users";
     }
 }
